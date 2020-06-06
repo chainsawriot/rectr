9e9335fd45538d89062bf90732a0e55b6018069b
chainsawriot <chainsawtiney@gmail.com>
2020-06-06 21:16:40 +0200

    Averaged Bert

---
+++ inst/python/bert.py
@@ -0,0 +1,50 @@
+import torch
+from transformers import *
+
+##model_class, tokenizer_class = (BertModel, BertTokenizer)
+
+##model = model_class.from_pretrained("bert-base-multilingual-cased")
+##tokenizer = tokenizer_class.from_pretrained("bert-base-multilingual-cased")
+
+def bert_word(text, model = BertModel.from_pretrained("bert-base-multilingual-cased"), tokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-cased")):
+    return(pipeline("feature-extraction", model = model, tokenizer = tokenizer)(text))
+
+# def bert_sentence(text, model = BertModel.from_pretrained("bert-base-multilingual-cased"), tokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-cased"), max_length = 512):
+#     input_ids = tokenizer.encode(text, add_special_tokens = True, max_length = max_length, return_tensors = 'pt')		
+#     with torch.no_grad():
+#         output_tuple = model(input_ids)
+#     output = output_tuple[0].squeeze()
+#     output = output.mean(dim = 0)
+#     output = output.numpy()
+#     return(output)
+
+def bert_sentence(content, model = BertModel.from_pretrained("bert-base-multilingual-cased"), tokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-cased"), max_length = 512, noise = True):
+    """Assuming content to be a vector of text from R"""
+    sentence_tensors = []
+    if noise:
+        print(content[0][0:30])
+    for text in content:
+        input_ids = tokenizer.encode(text, add_special_tokens = True, max_length = max_length, return_tensors = 'pt')		
+        with torch.no_grad():
+            output_tuple = model(input_ids)
+        output = output_tuple[0].squeeze()
+        sentence_tensors.append(output)
+    emb_torch = torch.cat(sentence_tensors, 0)
+    if noise:
+        print(emb_torch.size())
+    doc_emb = emb_torch.mean(dim = 0)
+    res = doc_emb.numpy()
+    return(res)
+
+def bert_corpus(corpus, model = BertModel.from_pretrained("bert-base-multilingual-cased"), tokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-cased"), max_length = 512):
+    res = []
+    for content in corpus:
+        output = bert_sentence(content = content, model = model, tokenizer = tokenizer, max_length = max_length)
+        res.append(output)
+    return(res)
+
+def bert_tokenize(text, tokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-cased")):
+    return tokenizer.encode(text, add_special_tokens = False)
+
+# def masked(text, model = BertModel.from_pretrained("bert-base-multilingual-cased"), tokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-cased")):
+#     return(pipeline("fill-mask", model = model, tokenizer = tokenizer)(text))
